c808cf734de27c9a5108f1531baa184582d241c6
lololokoko452 <mano.raichon@orange.fr>
2023-10-04 12:12:51 +0200

    Formulaire modification de mot de passe et modification user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserPasswordType;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * This controller alow us to edit User profile
+     *
+     * @param User $choosenUser
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET','POST'])]
+    #[Security("is_granted('ROLE_USER') and user === choosenUser")]
+    public function edit(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(UserType::class, $choosenUser);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            if ($hasher->isPasswordValid($choosenUser, $form->getData()->getPlainPassword()))
+            {
+                $user = $form->getData();
+                $manager->persist($user);
+                $manager->flush();
+                $this->addFlash('success', 'Utilisateur modifié !');
+                return $this->redirectToRoute('home.index');
+            }
+            else
+            {
+                $this->addFlash('warning', 'Mot de passe incorect');
+            }
+
+
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/utilisateur.edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
+    #[Security("is_granted('ROLE_USER') and user === choosenUser")]
+    public function editPassword(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher):Response
+    {
+        $form = $this->createForm(UserPasswordType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            if ($hasher->isPasswordValid($choosenUser, $form->getData()['plainPassword']))
+            {
+                $choosenUser->setCreatedAt(new \DateTimeImmutable());
+                $choosenUser->setPlainPassword(
+                    $form->getData()['newPassword']
+                );
+                $manager->persist($choosenUser);
+                $manager->flush();
+
+                $this->addFlash('success', 'Mot de passe modifié !');
+                return $this->redirectToRoute('home.index');
+            }else{
+                $this->addFlash('warning', 'Mote de passe incorrect ');
+            }
+        }
+        return $this->render('user/edit_password.html.twig', ['form' => $form]);
+    }
+}
