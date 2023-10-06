3ab1abe4e630458dcac7f0011a075f2b06a6bb78
lololokoko452 <mano.raichon@orange.fr>
2023-10-06 11:00:01 +0200

    POssibilité de reset le classement

--- src/Controller/ClassementController.php
+++ src/Controller/ClassementController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ClassementController extends AbstractController
 {
@@ -19,5 +22,32 @@ class ClassementController extends AbstractController
         return $this->render('classement/index.html.twig', [
             'users' => $users
         ]);
+    }
+
+    #[Route('/classement/reset', name: 'app.classement.reset')]
+    public function reset(UserRepository $userRepository, EntityManagerInterface $manager, TokenStorageInterface $tokenStorage): Response
+    {
+        if ($tokenStorage->getToken()->getUser()->getEmail() == "mano.raichon@orange.fr") {
+            $users = $userRepository->createQueryBuilder('u')
+                                ->getQuery()
+                                ->getResult();
+            foreach ($users as $user) {
+                $user->setScore(0);
+                $manager->persist($user);
+            }
+
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Classement réinitialisé'
+            );
+        } else {
+            $this->addFlash(
+                'error',
+                'Vous ne disposez pas des droits nécessaires.'
+            );
+        }
+        
+        return $this->redirectToRoute('home.index');
     }
 }
