6e9be7ab0a0ca11fcee718d72ef25fe3fec68c9d
lololokoko452 <mano.raichon@orange.fr>
2023-10-06 11:08:33 +0200

    Restriction des routes modif user / password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserController extends AbstractController
 {
@@ -25,26 +26,29 @@ class UserController extends AbstractController
      */
     #[Route('/utilisateur/edition/{id}', name: 'user.edit', methods: ['GET','POST'])]
     #[Security("is_granted('ROLE_USER') and user === choosenUser")]
-    public function edit(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    public function edit(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher, TokenStorageInterface $tokenStorage): Response
     {
-        $form = $this->createForm(UserType::class, $choosenUser);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            if ($hasher->isPasswordValid($choosenUser, $form->getData()->getPlainPassword()))
+        if ($tokenStorage->getToken()->getUser() == $choosenUser) {
+            $form = $this->createForm(UserType::class, $choosenUser);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid())
             {
-                $user = $form->getData();
-                $manager->persist($user);
-                $manager->flush();
-                $this->addFlash('success', 'Utilisateur modifié !');
-                return $this->redirectToRoute('home.index');
+                if ($hasher->isPasswordValid($choosenUser, $form->getData()->getPlainPassword()))
+                {
+                    $user = $form->getData();
+                    $manager->persist($user);
+                    $manager->flush();
+                    $this->addFlash('success', 'Utilisateur modifié !');
+                    return $this->redirectToRoute('home.index');
+                }
+                else
+                {
+                    $this->addFlash('warning', 'Mot de passe incorect');
+                }
             }
-            else
-            {
-                $this->addFlash('warning', 'Mot de passe incorect');
-            }
-
-
+        } else {
+            $this->addFlash('error', 'Vous ne disposez pas des autorisations');
+            return $this->redirectToRoute('home.index');
         }
 
         return $this->render('user/edit.html.twig', [
@@ -54,26 +58,30 @@ class UserController extends AbstractController
 
     #[Route('/utilisateur.edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
     #[Security("is_granted('ROLE_USER') and user === choosenUser")]
-    public function editPassword(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher):Response
+    public function editPassword(User $choosenUser, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher, TokenStorageInterface $tokenStorage):Response
     {
-        $form = $this->createForm(UserPasswordType::class);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            if ($hasher->isPasswordValid($choosenUser, $form->getData()['plainPassword']))
-            {
-                $choosenUser->setCreatedAt(new \DateTimeImmutable());
-                $choosenUser->setPlainPassword(
-                    $form->getData()['newPassword']
-                );
-                $manager->persist($choosenUser);
-                $manager->flush();
+        if ($tokenStorage->getToken()->getUser() == $choosenUser) {
+            $form = $this->createForm(UserPasswordType::class);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                if ($hasher->isPasswordValid($choosenUser, $form->getData()['plainPassword']))
+                {
+                    $choosenUser->setCreatedAt(new \DateTimeImmutable());
+                    $choosenUser->setPlainPassword(
+                        $form->getData()['newPassword']
+                    );
+                    $manager->persist($choosenUser);
+                    $manager->flush();
 
-                $this->addFlash('success', 'Mot de passe modifié !');
-                return $this->redirectToRoute('home.index');
-            }else{
-                $this->addFlash('warning', 'Mote de passe incorrect ');
+                    $this->addFlash('success', 'Mot de passe modifié !');
+                    return $this->redirectToRoute('home.index');
+                }else{
+                    $this->addFlash('warning', 'Mote de passe incorrect ');
+                }
             }
+        } else {
+            $this->addFlash('error', 'Vous ne disposez pas des autorisations');
+            return $this->redirectToRoute('home.index');
         }
         return $this->render('user/edit_password.html.twig', ['form' => $form]);
     }
